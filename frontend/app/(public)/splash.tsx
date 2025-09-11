603b60056d2c2e4f066c11868b11904269cd1110
오지환 <ohjihwan@naver.com>
2025-09-11 09:49:37 +0900

    스플레쉬 작업중

--- frontend/app/(public)/splash.tsx
+++ frontend/app/(public)/splash.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import { router } from "expo-router";
 import { getToken } from "../../lib/auth";
 import Animated, { Keyframe } from "react-native-reanimated";
@@ -18,7 +18,13 @@ const LogoImageAni = new Keyframe({
 	20:  { transform: [{ scaleX: 1.1 }, { scaleY: 0.2 }] },
 	80:  { transform: [{ scaleX: 1 }, { scaleY: 0.8 }] },
 	100: { transform: [{ scaleX: 1 }, { scaleY: 1 }] },
-}).duration(1000).delay(800)
+}).duration(600).delay(800)
+
+const windowHei = Dimensions.get("window").height + 50;
+const opningShow = new Keyframe({
+	0:   { transform: [{ translateY: -windowHei }] },
+	100: { transform: [{ translateY: 0 }] },
+}).duration(600).delay(1600)
 
 export default function SplashScreen() {
 	useEffect(() => {
@@ -45,6 +51,10 @@ export default function SplashScreen() {
 				style={styles.opningText} 
 				resizeMode="contain" 
 			/>
+			<Animated.View
+				entering={opningShow}
+				style={styles.opningShowingBox} 
+			/>
 		</View>
 	);
 }
@@ -59,7 +69,7 @@ const styles = StyleSheet.create({
 		top:0,
 		width:'100%',
 		height:'100%',
-		backgroundColor:'red'
+		paddingBottom:100,
 	},
 	opningImage: {
 		width: 140,
@@ -72,4 +82,12 @@ const styles = StyleSheet.create({
 		height: 50,
 		transformOrigin:'right',
 	},
+	opningShowingBox: {
+		position:'absolute',
+		left:0,
+		top:0,
+		width:'100%',
+		height:'100%',
+		backgroundColor:'green',
+	}
 });
