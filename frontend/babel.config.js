074d7d9452510f4fd54febe7582ab88ca61174ae
오지환 <ohjihwan@naver.com>
2025-09-15 17:49:47 +0900

    54버전으로 업그레이드 완료

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,10 @@
-// https://docs.expo.dev/guides/using-eslint/
+// frontend\babel.config.js
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            'expo-router/babel',
-            // Reanimated 플러그인은 항상 마지막
-            'react-native-reanimated/plugin',
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-worklets/plugin', // ← 마지막
+    ],
+  };
 };
