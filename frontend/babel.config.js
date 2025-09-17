a99686544f650dc5321ec5cb1fadd38c3b58168c
pigg4949 <pigg4949@gmail.com>
2025-09-17 16:40:57 +0900

    프론트, 백 서버 열리는거 확인

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,10 @@
-// frontend\babel.config.js
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'react-native-worklets/plugin', // ← 마지막
+      "react-native-worklets/plugin", // ← reanimated/plugin 대신 이걸 사용
     ],
   };
 };
