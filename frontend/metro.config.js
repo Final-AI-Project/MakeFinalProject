04970e00188e15bf1449fa326d9856d72f692070
오지환 <ovitwisdom@gmail.com>
2025-09-22 21:17:57 +0900

    모델 연동 test

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -4,5 +4,6 @@ const config = getDefaultConfig(__dirname);
 
 // common 폴더를 라우트에서 제외
 config.resolver.platforms = ["ios", "android", "native", "web"];
+config.resolver.assetExts.push("ptl", "txt");
 
 module.exports = config;
